a6d3a3985f935c546d17bb2192bf461a46b3d40f
Thibault Clérice <leponteineptique@gmail.com>
2019-11-14 17:52:29 +0100

    A little more of computing

--- lemma_pos_diff.py
+++ lemma_pos_diff.py
@@ -6,7 +6,7 @@ import statistics
 def geomean(xs):
     return math.exp(math.fsum(math.log(x) for x in xs) / len(xs))
 
-counts = collections.defaultdict(lambda: {"ocr": 0, "raw": 0})
+counts = collections.defaultdict(lambda: {"ocr": [], "gt": []})
 
 
 with open("lemma.csv") as f:
@@ -14,7 +14,16 @@ with open("lemma.csv") as f:
 
 	titles = []
 	types = []
-
+	decompte = {
+		"total": {
+			"ocr": 0,
+			"gt": 0
+		},
+		"specifique": {
+			"ocr": 0,
+			"gt": 0
+		}
+	}
 	apaxes = {"ocr": 0, "gt": 0, "same": 0}
 	totals = {"ocr": 0, "gt": 0}
 
@@ -50,39 +59,52 @@ with open("lemma.csv") as f:
 	f.seek(0)
 	for num, line in enumerate(reader):
 		if num == 0:
-			titles = ["lemma"] + line[1:]
+			titles = line[1:]
 		elif num == 1:
 			types = line[1:]
 		else:
-			by_types = zip(types, line[1:])
+			by_types = zip(titles, types, line[1:])
 
-			here = {"ocr": 0, "gt": 0}
+			here = {title: {"ocr": 0, "gt": 0} for title in titles}
+			here["total"] = {"ocr": 0, "gt": 0}
 
-			for type_name, cnt in by_types:
+
+			for title, type_name, cnt in by_types:
 				totals[type_name] += int(cnt)
-				here[type_name] += int(cnt)
+
+				here[title][type_name] += int(cnt)
+				here["total"][type_name] += int(cnt)
 
 			# Diff relative générale
-			freqs = {key: val/totals[key] for key, val in here.items()}
-			rel = abs(freqs["gt"] - freqs["ocr"]) / max(list(freqs.values()))
+			for title in here:
+				freqs = {
+					key: val/totals[key] 
+					for key, val in here[title].items()
+				}
+				maximum_freq = max(list(freqs.values()))
 
-			if rel == 1.0:
-				print(list(zip(types, line[1:])))
+				if maximum_freq > 0.:
+					rel = abs(freqs["gt"] - freqs["ocr"]) / maximum_freq
 
-			# If 1.0, apaxe de type (présent que dans OCR ou que dans GT)
-			if rel != 1.0:
-				diff_moy_by_title_hors_apaxes_de_type["total"].append(rel)
+					# If 1.0, apaxe de type (présent que dans OCR ou que dans GT)
+					if rel != 1.0:
+						diff_moy_by_title_hors_apaxes_de_type[title].append(rel)
+					
+					if title == "total":
+						decompte["total"]["ocr"] += here["total"]["ocr"]
+						decompte["total"]["gt"] += here["total"]["gt"]
 
-			diff_moy_by_title["total"].append(rel)
+
+					diff_moy_by_title[title].append(rel)
 
 
 			# Decompte des apaxes pour le total
 			for key in ["gt", "ocr"]:
-				if here[key] == 1:
+				if here["total"][key] == 1:
 					apaxes[key] += 1
 
 			# Decompte des apaxes retrouvés dans les deux documents
-			if here["gt"] == here["ocr"]:
+			if here["total"]["gt"] == here["total"]["ocr"]:
 				apaxes["same"] += 1
 
 	for key in diff_moy_by_title:
@@ -101,5 +123,16 @@ with open("lemma.csv") as f:
 				"geo_mean" : geomean(diff_moy_by_title_hors_apaxes_de_type[key]),
 				"median": statistics.median(diff_moy_by_title_hors_apaxes_de_type[key])
 			}
-	print(apaxes) # {'ocr': 2353, 'gt': 1052, 'same': 871}
-	print(diff_moy_by_title_hors_apaxes_de_type)
+
+from pprint import pprint
+print("======")
+print("Apaxes")	
+print(apaxes) # {'ocr': 2353, 'gt': 1052, 'same': 871}
+print("======")
+print("Diff moy out of apaxes")
+pprint(diff_moy_by_title_hors_apaxes_de_type)
+print("======")
+print("Diff moy ")
+pprint(diff_moy_by_title)
+
+pprint(decompte)
